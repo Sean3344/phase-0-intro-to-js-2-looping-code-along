01fa97accc68fed4b4cd8fa0d90638c9ae10dabd
Sean Nekouei <sean.nekouei@gmail.com>
2022-12-16 13:20:36 -0700

    commit tree c8f83514935259931634e0908e16c9820b0b2851

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+
+
+function writeCards(name, gift) {
+    let thearray = []
+    for(let i = 0;i< name.length;i++) {
+        thearray.push(`Thank you, ${name[i]}, for the wonderful ${gift} gift!`)
+         }
+         return thearray
+}
+function countDown(number) {
+    while (number > (-1)) {
+      console.log(number--);
+      }
+    }
+    countDown(10)
